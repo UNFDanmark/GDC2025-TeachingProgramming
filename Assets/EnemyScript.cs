8c97d74f508164f971354bff94914a04a431035e
Baltzer77 <46567276+Baltzer77@users.noreply.github.com>
2025-07-09 09:16:44 +0200

    Dag 1 del 2 færdig

--- Assets/EnemyScript.cs
+++ Assets/EnemyScript.cs
@@ -5,7 +5,7 @@ public class EnemyScript : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        print("Hej jeg er ond");       
+        print("Hej jeg er ond");
     }
 
     // Update is called once per frame
--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -1,16 +1,24 @@
+using Unity.Collections;
 using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
+    public int health = 10;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        print("Jeg er kaldt en gang");
+        int jegVirkerKunIStart = 5;
+        float etDecimalTal = 13.7f;
+        
+        
+        print(jegVirkerKunIStart + health);
     }
 
     // Update is called once per frame
     void Update()
     {
-        print("Jeg er kaldt meget");
+        int jegVirkerKunIUpdate = 2;
+        print(health + jegVirkerKunIUpdate);
     }
 }
