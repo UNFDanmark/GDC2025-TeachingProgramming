d45836186c4f28273275fdb49337fea03c8f027a
Baltzer77 <46567276+Baltzer77@users.noreply.github.com>
2025-07-09 13:38:20 +0200

    RotateScript

--- Assets/Scripts/RotateScript.cs
+++ Assets/Scripts/RotateScript.cs
@@ -1,16 +1,18 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class RotateScript : MonoBehaviour
 {
+    public InputAction turnAround;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        turnAround.Enable();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(0, turnAround.ReadValue<float>(), 0);
     }
 }
