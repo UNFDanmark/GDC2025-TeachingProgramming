cb9838bd579b726daf33e905a6bb97983f718400
Baltzer77 <46567276+Baltzer77@users.noreply.github.com>
2025-07-09 09:38:21 +0200

    Dag 1 del 3

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -1,24 +1,31 @@
 using Unity.Collections;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerScript : MonoBehaviour
 {
-    public int health = 10;
-    
+    public int speed = 18;
+    public InputAction moveAction;
+    Rigidbody rb;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        int jegVirkerKunIStart = 5;
-        float etDecimalTal = 13.7f;
-        
-        
-        print(jegVirkerKunIStart + health);
+        rb = GetComponent<Rigidbody>();
+        moveAction.Enable();
     }
 
     // Update is called once per frame
     void Update()
     {
-        int jegVirkerKunIUpdate = 2;
-        print(health + jegVirkerKunIUpdate);
+        // læs input
+        Vector2 moveInput = moveAction.ReadValue<Vector2>();
+
+        // skab kopi
+        Vector3 newVelocity = rb.linearVelocity;
+        // opdaterer vi værdier
+        newVelocity.x = moveInput.x * speed;
+        newVelocity.z = moveInput.y * speed;
+        // Opdater den rigtige hastighed
+        rb.linearVelocity = newVelocity;
     }
 }
