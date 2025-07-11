5b3a5ccbac713b4e48d01743841ef514197fdee4
Baltzer77 <46567276+Baltzer77@users.noreply.github.com>
2025-07-11 14:21:52 +0200

    Del 1 færdig

---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    // Dette er en kommentar
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        print("Hej fra start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print("Hej fra update");
+    }
+}
